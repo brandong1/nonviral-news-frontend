08a6284fae71bcf16a577a442ae0599e781a1504
github username <github email address>
2020-03-25 11:32:41 -0600

    Add initial showArticle functionality

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-fetch('http://localhost:3000/articles')
-    .then(response => response.json())
-    .then(console.log)
+fetch("http://localhost:3000/articles")
+  .then(response => response.json())
+  .then(articles => showArticle(articles))
+
+function showArticle(articles) {
+  const newsFeed = document.getElementById("news-feed")
+
+  articles.forEach(article => {
+    const newArticle = document.createElement("article")
+    newsFeed.append(newArticle)
+
+    const title = document.createElement("h2")
+    title.innerText = article.title
+    newArticle.append(title)
+  })
+}
